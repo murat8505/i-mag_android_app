a2859a1e270f2c2c1832433c58a2386945c548ad
sergius-m <p37sergius@gmail.com>
2013-12-09 16:07:08 +0300

    Added custom cursor adapter

--- src/by/imag/app/FragmentListTags.java
+++ src/by/imag/app/FragmentListTags.java
@@ -1,7 +1,29 @@
 package by.imag.app;
 
+import android.content.Context;
+import android.database.Cursor;
 import android.support.v4.app.ListFragment;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
 
 public class FragmentListTags extends ListFragment{
     //
+
+    class TagCursorAdapter extends CursorAdapter {
+
+        public TagCursorAdapter(Context context, Cursor c, boolean autoRequery) {
+            super(context, c, autoRequery);
+        }
+
+        @Override
+        public View newView(Context context, Cursor cursor, ViewGroup parent) {
+            return null;
+        }
+
+        @Override
+        public void bindView(View view, Context context, Cursor cursor) {
+
+        }
+    }
 }
