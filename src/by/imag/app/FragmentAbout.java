47cd3cb1491b87156e59d6d155cc15bbf1a8f3eb
sergius-m <p37sergius@gmail.com>
2014-02-04 18:23:03 +0300

    Renamed file fragment_about

--- src/by/imag/app/FragmentAbout.java
+++ src/by/imag/app/FragmentAbout.java
@@ -9,7 +9,7 @@ public class FragmentAbout extends BaseFragment{
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.about_frag, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_about, container, false);
         setActionBarSubtitle(4);
         return rootView;
     }
